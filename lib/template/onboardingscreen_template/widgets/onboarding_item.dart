6e7146da767f41a6d7f3aab75bf70283b7a22eeb
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-03-21 10:10:08 +0700

    re-write onboarding item

--- lib/template/onboardingscreen_template/widgets/onboarding_item.dart
+++ lib/template/onboardingscreen_template/widgets/onboarding_item.dart
@@ -4,22 +4,30 @@ import 'package:global_template/global_template.dart';
 class OnboardingItem extends StatelessWidget {
   const OnboardingItem({
     Key? key,
-    this.titleAlign = Alignment.center,
-    this.titlePadding = const EdgeInsets.all(8),
-    this.subtitleAlign,
-    this.subtitlePadding,
+    this.titlePadding = const EdgeInsets.symmetric(horizontal: 12),
+    this.subtitlePadding = const EdgeInsets.symmetric(horizontal: 12),
     this.logo,
-    this.title,
-    this.subtitle,
+    this.title = 'Default title',
+    this.subtitle = 'Default Subtitle',
+    this.textTitleAlign = TextAlign.center,
+    this.textSubtitleAlign = TextAlign.center,
+    this.titleStyle,
+    this.subtitleStyle,
+    this.titleAlign = Alignment.center,
+    this.subtitleAlign = Alignment.center,
   }) : super(key: key);
 
-  final AlignmentGeometry titleAlign;
   final EdgeInsetsGeometry titlePadding;
+  final EdgeInsetsGeometry subtitlePadding;
+  final TextAlign textTitleAlign;
+  final TextAlign textSubtitleAlign;
+  final AlignmentGeometry? titleAlign;
   final AlignmentGeometry? subtitleAlign;
-  final EdgeInsetsGeometry? subtitlePadding;
   final Widget? logo;
-  final Widget? title;
-  final Widget? subtitle;
+  final TextStyle? titleStyle;
+  final TextStyle? subtitleStyle;
+  final String title;
+  final String subtitle;
 
   @override
   Widget build(BuildContext context) {
@@ -27,28 +35,34 @@ class OnboardingItem extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Expanded(
-          flex: 6,
+          flex: 8,
           child: Center(
-            child: logo ??
-                Icon(
-                  Icons.design_services,
-                  size: sizes.width(context),
-                ),
+            child: logo ?? Icon(Icons.design_services, size: sizes.width(context)),
           ),
         ),
         Expanded(
-          flex: 2,
           child: Container(
             padding: titlePadding,
             alignment: titleAlign,
-            child: title,
+            child: Text(
+              title,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              textAlign: textTitleAlign,
+              style: titleStyle,
+            ),
           ),
         ),
         Expanded(
+          flex: 3,
           child: Container(
             padding: subtitlePadding,
             alignment: subtitleAlign,
-            child: subtitle,
+            child: Text(
+              subtitle,
+              textAlign: textSubtitleAlign,
+              style: subtitleStyle,
+            ),
           ),
         ),
       ],
