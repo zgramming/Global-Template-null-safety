606db4d82f591910b8af323479fcc3333c8b1f85
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-06-21 17:22:14 +0700

    add function listOfMonth & listOfYear

--- lib/functions/global_function.dart
+++ lib/functions/global_function.dart
@@ -598,6 +598,32 @@ class GlobalFunction {
     );
   }
 
+  static Map<int, String> listOfMonth() {
+    return {
+      1: 'Januari',
+      2: 'Februari',
+      3: 'Maret',
+      4: 'April',
+      5: 'Mei',
+      6: 'Juni',
+      7: 'Juli',
+      8: 'Agustus',
+      9: 'September',
+      10: 'Oktober',
+      11: 'November',
+      12: 'Desember',
+    };
+  }
+
+  static Map<int, int> listOfYear({
+    required int from,
+    required int to,
+  }) {
+    return {
+      for (var i = from; i <= to; i++) i: i,
+    };
+  }
+
   ///* Check if value in list already exist/not
   static T? isValueExistObject<T>(
     List<T> checkedList,
