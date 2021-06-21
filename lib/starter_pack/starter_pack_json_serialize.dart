518dd36a796b97ff4cb35695242900e1c4fbda9d
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-06-21 16:54:07 +0700

    starter pack json serialize

---
+++ lib/starter_pack/starter_pack_json_serialize.dart
@@ -0,0 +1,20 @@
+// import 'package:equatable/equatable.dart';
+// import 'package:flutter/material.dart';
+// import 'package:global_template/global_template.dart';
+// import 'package:json_annotation/json_annotation.dart';
+
+// part 'sales_model.g.dart';
+
+// @immutable
+// @JsonSerializable(fieldRename: FieldRename.snake)
+
+// class SalesModel extends Equatable{
+
+// }
+// @JsonKey(
+//     toJson: GlobalFunction.toJsonStringFromInteger,
+//     fromJson: GlobalFunction.fromJsonStringToInteger,
+//   )
+
+// factory UserModel.fromJson(Map<String, dynamic> json) => _$UserModelFromJson(json);
+// Map<String, dynamic> toJson() => _$UserModelToJson(this);
